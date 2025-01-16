38e24c470b960cf5c3b2c9b827835bc31236bfb8
Jiwon Park <wldnjs1123@gmail.com>
2025-01-16 13:12:04 +0900

    [init] 프로젝트 생성

---
+++ src/test/java/com/zziony/Davinci/DavinciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zziony.Davinci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavinciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
