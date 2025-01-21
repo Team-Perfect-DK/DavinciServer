80d575486f84839e1342d165ab35911d2880db9b
JH <ubuntu@ip-172-31-35-42.ap-northeast-2.compute.internal>
2025-01-21 07:08:58 +0000

    [feature] 테스트 API 추가

---
+++ src/main/java/com/zziony/Davinci/DavinciController.java
@@ -0,0 +1,16 @@
+package com.zziony.Davinci;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class DavinciController {
+
+    @GetMapping("/v1/davinci")
+    public String davinci() {
+        return "오징어 게임을 시작합니다.";
+    }
+
+}
