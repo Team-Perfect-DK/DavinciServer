ff6f707e382facbcbf84b1778caecf6c3e8b001d
Jiwon Park <wldnjs1123@gmail.com>
2025-06-11 12:38:34 +0900

    [feat] userController register 변경

--- src/main/java/com/zziony/Davinci/controller/UserController.java
+++ src/main/java/com/zziony/Davinci/controller/UserController.java
@@ -2,9 +2,13 @@ package com.zziony.Davinci.controller;
 
 import com.zziony.Davinci.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.Map;
 
 
@@ -15,13 +19,17 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/register")
-    public Map<String, String> registerUser(@RequestBody Map<String, String> request) {
-        String nickname = request.get("nickname");
-        String sessionId = userService.registerUser(nickname);
 
-        Map<String, String> response = new HashMap<>();
-        response.put("sessionId", sessionId);
-        return response;
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody Map<String, String> request) {
+        try {
+            String nickname = request.get("nickname");
+            String sessionId = userService.registerUser(nickname);
+            return ResponseEntity.ok(Map.of("sessionId", sessionId));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(Map.of("error", e.getMessage()));
+        }
     }
+
 }
